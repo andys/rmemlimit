b10c5dae972073024e6d53de5210777372bf01e4
Andrew Snow <andrew@modulus.org>
2013-08-15 13:36:33 +1000

    only stop GC if gc_mb is set

--- lib/rmemlimit.rb
+++ lib/rmemlimit.rb
@@ -1,53 +1,55 @@
-
 class Rmemlimit
 
   class << self
-    attr_accessor :gc_mb, :kill_mb
-    
+    attr_accessor :gc_mb, :kill_mb, :sleep_time
+
     def gcthread
       if !@gcthread || !@gcthread.alive?
         @gcthread = Thread.new do
-          loop do
-            mb = rss_mb
-            if kill_mb && mb > kill_mb
-              STDERR.puts "#{self}: Exceeded kill memory limit (#{mb} > #{kill_Mb} MB)"
-              Process.kill(9, $$)
-            elsif gc_mb && mb > gc_mb
-              run_gc
-            elsif mb == 0 || !gc_mb || !kill_mb
-              run_gc
+          begin
+            loop do
+              mb = rss_mb
+              if kill_mb && mb > kill_mb
+                STDERR.puts "#{self}: Exceeded kill memory limit (#{mb} > #{kill_mb} MB)"
+                Process.kill(9, $$)
+              elsif gc_mb && mb > gc_mb
+                run_gc
+              end
+              sleep sleep_time
             end
-            sleep 1
+          rescue
+            puts "Exception: #{$!.class} #{$!}"
           end
         end
       end
     end
-    
+
     def run_gc
       GC.enable
       GC.start
       GC.disable
     end
-  
+
     def pagesize
       4096
     end
-    
+
     def rss_mb
       rss_kb >> 10
     end
-    
+
     def rss_kb
       pagesize * (File.read("/proc/#{$$}/statm").split(' ')[1].to_i rescue 0) >> 10
     end
-    
+
     def setup
+      self.sleep_time ||= 1
       self.gc_mb = ENV['RUBY_GC_MB'].to_i if ENV['RUBY_GC_MB']
       if ENV['RUBY_KILL_MB']
-        self.kill_mb = ENV['RUBY_KILL_MB'].to_i 
+        self.kill_mb = ENV['RUBY_KILL_MB'].to_i
         self.kill_mb = nil unless Rmemlimit.kill_mb > 1
       end
-      GC.disable
+      GC.disable if gc_mb
       self.gcthread
     end
   end
--- test/test_all.rb
+++ test/test_all.rb
@@ -5,6 +5,7 @@ require 'test/unit'
 class TestRmemlimit < Test::Unit::TestCase
 
   def test_rss_kb
+    Rmemlimit.run_gc
     kb = Rmemlimit.rss_kb
     assert(kb > 0)
     
@@ -12,18 +13,13 @@ class TestRmemlimit < Test::Unit::TestCase
                  File.read("/proc/#{$$}/status").lines.grep(/VmRSS/).first
   end
   
-  def test_run_gc
-    gccount = GC.stat[:count] + 1
-    sleep 1.1
-    assert_equal gccount, GC.stat[:count]
-  end
-  
   def test_gc_mb
     Rmemlimit.gc_mb = Rmemlimit.rss_mb
-    gccount = GC.stat[:count] + 1
-    flibble = 'x' * 1024 * 1024
-    sleep 1.1
-    assert_equal gccount, GC.stat[:count]
+    Rmemlimit.sleep_time = 0.5
+    gccount = GC.stat[:count]
+    flibble = 'x' * 100000000
+    sleep 0.6
+    assert_equal gccount + 1, GC.stat[:count] 
   end
 
 end
